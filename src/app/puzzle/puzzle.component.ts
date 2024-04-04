79c15c4350d091a05d86c3d745c9aa2576d2f650
John Nguyen <johnnguyen4501@gmail.com>
2024-04-04 14:45:28 -0400

    fix puzzle

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -22,24 +22,30 @@ import { CommonModule } from '@angular/common';
     CommonModule,
   ],
   templateUrl: './puzzle.component.html',
-  styleUrl: './puzzle.component.css'
+  styleUrl: './puzzle.component.css',
 })
 export class PuzzleComponent {
-  list1: string[] = ['5','3','6','4'];
-  list2: string[] = ['9','2','7','15'];
-  list3: string[] = ['14','10','x','11'];
-  list4: string[] = ['1','13','12','8'];
+  tiles: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 11, 12, 13, 14, 15];
 
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      if (event.container.data[event.previousIndex]=='x' || event.container.data[event.currentIndex]=='x'){
-        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      }
-    } else {
-      if ((event.previousIndex==event.currentIndex) && (event.previousContainer.data[event.previousIndex]=='x' || event.container.data[event.currentIndex]=='x')){
-        transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-        transferArrayItem(event.container.data, event.previousContainer.data, event.currentIndex+1, event.previousIndex);
-      }
+  drop(event: CdkDragDrop<number>) {
+    let draggedIndex = event.previousContainer.data;
+    let destinationIndex = event.container.data;
+
+    let dragged = this.tiles[event.previousContainer.data];
+    let destination = this.tiles[event.container.data];
+
+    if (destination == 0 && this.isAdjacent(draggedIndex, destinationIndex)) {
+      this.tiles[destinationIndex] = dragged;
+      this.tiles[draggedIndex] = 0;
     }
+  }
+
+  isAdjacent(draggedIndex: any, destinationIndex: any): boolean {
+    return (
+      this.tiles[draggedIndex] == this.tiles[destinationIndex + 1] ||
+      this.tiles[draggedIndex] == this.tiles[destinationIndex - 1] ||
+      this.tiles[draggedIndex] == this.tiles[destinationIndex + 4] ||
+      this.tiles[draggedIndex] == this.tiles[destinationIndex - 4]
+    );
   }
 }
